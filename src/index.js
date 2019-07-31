e78c6469c2038fd8e11bd441acc2010f7b0e2223
Imonk <tangzheng1900@163.com>
2019-07-31 17:02:46 +0800

    common: 测试函数组件

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const A = ()=>1;
 function App() {
   return (
     <div className="App">
+    {A()}
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
